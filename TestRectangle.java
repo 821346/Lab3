6f12a52dc63ccda5e0295ce63d3b7ffd5615b713
821346 <98726531+821346@users.noreply.github.com>
2022-01-31 21:00:45 -0500

    Create TestRectangle.java

---
+++ TestRectangle.java
@@ -0,0 +1,16 @@
+
+public class TestRectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//rectangle objects rect1 and rect2
+		//use system.out.println to print the info for both rectangles
+		//area
+		Rectangle rect1=new Rectangle(-8, 0, 6.2, 3.1);
+		Rectangle rect2=new Rectangle(-2, -3, 4, 8);
+		System.out.println(rect1.toString());
+		
+		System.out.println(rect2.toString());
+	}
+
+}
